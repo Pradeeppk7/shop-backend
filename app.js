2bd84b349ed397d86e78cb6261aa4057a65c238c
“[Pradeep <[pradeepkumar.pk002@gmail.com]>
2022-10-18 23:27:21 +0530

    installed morgan

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
-const express = require('express');
+const express = require(
+    "express");
 const app = express();
-
-require('dotenv/config');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+require("dotenv/config");
 
 const api = process.env.API_URL;
-app.get(api+'/products', (req, res) => {
-    res.send('hello API !');
+//middleware
+//app.use(express.json());
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+
+app.get(`${api}/products`, (req, res) => {
+    const product = {
+        id: 1,
+        name: "hair dresser",
+        image:'someurl',
+    }
+    res.send(product);
+})
+app.post(`${api}/products`, (req, res) => {
+    const newProduct = req.body;
+    console.log(newProduct);
+    res.send(newProduct);
 })
 app.listen(3000, () => {
     console.log(api);
