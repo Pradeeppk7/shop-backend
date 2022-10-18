5f000af161d0be8a2206355dd50c949dfc1058ef
“[Pradeep <[pradeepkumar.pk002@gmail.com]>
2022-10-18 22:54:26 +0530

    HELLO API installed express

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("hello shop");
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+    res.send('hello API !');
+})
+app.listen(3000, () => {
+    console.log('sever is running');
+})
