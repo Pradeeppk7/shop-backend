7d699eeef82a93f7aa3cb82c19d16914beb72690
“[Pradeep <[pradeepkumar.pk002@gmail.com]>
2022-10-19 23:27:26 +0530

    Read/Write api

--- app.js
+++ app.js
@@ -12,18 +12,40 @@ const api = process.env.API_URL;
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
-app.get(`${api}/products`, (req, res) => {
-    const product = {
-        id: 1,
-        name: "hair dresser",
-        image:'someurl',
+const productSchema = mongoose.Schema({
+    name: String,
+    image: String,
+    countInStock: {
+        type: Number,
+        required:true
     }
-    res.send(product);
+    
+})
+
+const Product = mongoose.model('Product', productSchema);
+
+app.get(`${api}/products`, async(req, res) => {
+    const productList = await Product.find();
+    if (!productList) {
+        res.status(500).json({success:false})
+    }
+    res.send(productList);
 })
 app.post(`${api}/products`, (req, res) => {
-    const newProduct = req.body;
-    console.log(newProduct);
-    res.send(newProduct);
+    const product = new Product({
+        name: req.body.name,
+        image: req.body.image,
+        countInStock:req.body.countInStock
+    })
+    product.save().then((createdProduct => {
+        res.status(201).json(createdProduct)
+    })).catch((err) => {
+        res.status(500).json({
+            error: err,
+            success:false
+        })
+    })
+    
 })
 mongoose.connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
