16e94ee2cbce0402c263b543be31dce39cb65ad2
“[Pradeep <[pradeepkumar.pk002@gmail.com]>
2022-10-19 23:48:20 +0530

    Model and router

--- app.js
+++ app.js
@@ -3,50 +3,23 @@ const express = require(
 const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-require("dotenv/config");
 const mongoose = require('mongoose');
 
+require("dotenv/config");
 const api = process.env.API_URL;
+
+const productsRouter = require('./routers/products');
 //middleware
 //app.use(express.json());
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
-const productSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: {
-        type: Number,
-        required:true
-    }
-    
-})
+//routers
+app.use(`${api}/products`,productsRouter)
 
-const Product = mongoose.model('Product', productSchema);
 
-app.get(`${api}/products`, async(req, res) => {
-    const productList = await Product.find();
-    if (!productList) {
-        res.status(500).json({success:false})
-    }
-    res.send(productList);
-})
-app.post(`${api}/products`, (req, res) => {
-    const product = new Product({
-        name: req.body.name,
-        image: req.body.image,
-        countInStock:req.body.countInStock
-    })
-    product.save().then((createdProduct => {
-        res.status(201).json(createdProduct)
-    })).catch((err) => {
-        res.status(500).json({
-            error: err,
-            success:false
-        })
-    })
-    
-})
+
+
 mongoose.connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
