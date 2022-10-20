8a7138a883a0e346f651968de6d5ad7d4c15820e
“[Pradeep <[pradeepkumar.pk002@gmail.com]>
2022-10-20 23:53:40 +0530

    Model update

--- models/category.js
+++ models/category.js
@@ -1,7 +1,26 @@
 const mongoose = require('mongoose');
 
 const categorySchema = mongoose.Schema({
-
+    name: {
+        type: String,
+        required: true,
+    },
+    icon: {
+        type: String,
+    },
+    color: { 
+        type: String,
+    }
 })
 
+//to make frontend friendly as we us id more often (_id to id)
+categorySchema.virtual('id').get(function () {
+    return this._id.toHexString();
+});
+
+categorySchema.set('toJSON', {
+    virtuals: true,
+});
+
 exports.Category = mongoose.model('Category', categorySchema);
+
